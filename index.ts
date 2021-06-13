e6e073e1a8d8a7d2a4a7aef88d023cb99b1349e9
JoseMarioDev <josemariodev@gmail.com>
2021-06-13 10:34:25 -0500

    :art: creating new role and lambda func

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
+
+// create role for permissions for your lambda func
+
+const role = new aws.iam.Role('my-function-role', {
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+    Service: 'lambda.amazonaws.com',
+  }),
+});
+
+//  lambda func that returns a hello msg
+
+const lambdaFunction = new aws.lambda.Function('my-function', {
+  role: role.arn,
+  handler: 'index.handler',
+  runtime: aws.lambda.NodeJS12dXRuntime,
+  code: new pulumi.asset.AssetArchive({
+    'index.js': new pulumi.asset.StringAsset(
+      'exports.handler = (e,c,cb) => cb(null,{statusCode:200, body:"hello world"});',
+    ),
+  }),
+});
